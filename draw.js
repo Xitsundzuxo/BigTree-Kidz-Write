37c0e40f27a1d462eaece212a665c0b79a344dc4
Xitsundzuxo <xitsundzuxomaluleke909@gmail.com>
2026-01-31 18:23:00 +0200

    Create draw.js

---
+++ draw.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById("drawArea");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 300;
+canvas.height = 300;
+
+ctx.lineWidth = 6;
+ctx.lineCap = "round";
+
+let drawing = false;
+
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mouseup", stopDraw);
+canvas.addEventListener("mousemove", draw);
+
+canvas.addEventListener("touchstart", startDraw);
+canvas.addEventListener("touchend", stopDraw);
+canvas.addEventListener("touchmove", draw);
+
+function startDraw(e) {
+  drawing = true;
+  ctx.beginPath();
+}
+
+function stopDraw() {
+  drawing = false;
+}
+
+function draw(e) {
+  if (!drawing) return;
+
+  const rect = canvas.getBoundingClientRect();
+  const x = (e.touches ? e.touches[0].clientX : e.clientX) - rect.left;
+  const y = (e.touches ? e.touches[0].clientY : e.clientY) - rect.top;
+
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
