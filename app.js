d19e406c26b8e94d893c22604e5eaae183b74dcd
Xitsundzuxo <xitsundzuxomaluleke909@gmail.com>
2026-01-31 18:35:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
 }
+
+function startLearning() {
+  document.getElementById("parentScreen").classList.add("hidden");
+  document.getElementById("kidScreen").classList.remove("hidden");
+}
